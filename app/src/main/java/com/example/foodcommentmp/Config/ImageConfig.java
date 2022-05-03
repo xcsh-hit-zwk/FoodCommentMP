e17f3093339109a7d407f239723bb3b037ca4267
xcsh <zhangweikun_xcsh@163.com>
2022-05-03 09:54:20 +0800

    完成了图片获取接口

---
+++ app/src/main/java/com/example/foodcommentmp/Config/ImageConfig.java
@@ -0,0 +1,13 @@
+package com.example.foodcommentmp.Config;
+
+import android.net.Uri;
+
+/**
+ * @author: zhangweikun
+ * @create: 2022-05-01 9:18
+ */
+public class ImageConfig {
+
+    public static final String DIR = "/storage/emulated/0/$MuMu共享文件夹";
+
+}
