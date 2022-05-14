84d998cd6d2c490207c5f948669a8d48afb28094
xcsh <zhangweikun_xcsh@163.com>
2022-05-14 16:28:03 +0800

    完成了历史搜索记录的客户端部分

--- app/src/main/java/com/example/foodcommentmp/fragments/RestaurantOverViewFragment.java
+++ app/src/main/java/com/example/foodcommentmp/fragments/RestaurantOverViewFragment.java
@@ -1,12 +1,10 @@
 package com.example.foodcommentmp.fragments;
 
-import androidx.appcompat.widget.SearchView;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 
@@ -23,14 +21,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListPopupWindow;
 import android.widget.Spinner;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import com.example.foodcommentmp.Activitys.RestaurantSearchActivity;
-import com.example.foodcommentmp.Adapters.AdminRestaurantInfoAdaptor;
 import com.example.foodcommentmp.Adapters.RestaurantOverViewAdapter;
 import com.example.foodcommentmp.Adapters.SpinnerAdaptor;
 import com.example.foodcommentmp.Config.ServerConfig;
@@ -44,8 +44,6 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-
-import javax.xml.transform.Result;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
@@ -63,10 +61,20 @@ public class RestaurantOverViewFragment extends Fragment {
     private EditText editText;
     private RecyclerView recyclerView;
     private RestaurantOverViewAdapter restaurantOverViewAdapter;
+    private ImageView searchIcon;
+    private ImageButton searchButton;
+
+    private ListPopupWindow listPopupWindow;
 
     private String chosen;
 
     private List<RestaurantOverView> restaurantOverViewList = new ArrayList<>();
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private static final int SHOW_HISTORY_COUNTS = 5;
+    private String[] historyList = new String[1];
 
     private static final String[] searchMod = new String[]{"城市", "街区", "招牌菜", "餐厅类型"};
 
@@ -79,6 +87,9 @@ public class RestaurantOverViewFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         mViewModel = new ViewModelProvider(this).get(RestaurantOverViewViewModel.class);
+
+        sharedPreferences = getContext().getSharedPreferences("restaurant_search_history", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
@@ -138,11 +149,30 @@ public class RestaurantOverViewFragment extends Fragment {
 
         spinner.setAdapter(new SpinnerAdaptor(searchMod, getContext()));
         linearLayout = getView().findViewById(R.id.search_background);
+        searchIcon = getView().findViewById(R.id.search_icon);
+        searchButton = getView().findViewById(R.id.search_image_button);
 
         GradientDrawable background = new GradientDrawable();
         background.setColor(getContext().getResources().getColor(R.color.lightgray));
         background.setCornerRadius(20);
         linearLayout.setBackground(background);
+
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHistory();
+                editText.setFocusable(true);
+                editText.setFocusableInTouchMode(true);
+            }
+        });
+        searchIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHistory();
+                editText.setFocusable(true);
+                editText.setFocusableInTouchMode(true);
+            }
+        });
 
         // 设置下拉框监听
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -159,20 +189,6 @@ public class RestaurantOverViewFragment extends Fragment {
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
                 chosen = searchMod[0];
-            }
-        });
-
-        linearLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getContext(), RestaurantSearchActivity.class));
-            }
-        });
-
-        editText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getContext(), RestaurantSearchActivity.class));
             }
         });
 
@@ -204,6 +220,56 @@ public class RestaurantOverViewFragment extends Fragment {
             }
         });
 
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // todo 这里要调用各种搜索接口，现在只是临时测试历史记录保存
+                int historyCounts = sharedPreferences.getInt("history_counts", 0);
+                String searchInfo = editText.getText().toString();
+                editor.putString("Key"+String.valueOf(historyCounts+1), searchInfo);
+                editor.remove("history_counts");
+                editor.putInt("history_counts", historyCounts+1);
+                editor.commit();
+            }
+        });
+
+    }
+
+    private void showHistory(){
+        int historyCounts = sharedPreferences.getInt("history_counts", 0);
+        if(historyCounts >= 5){
+            historyList = new String[6];
+            historyList[SHOW_HISTORY_COUNTS] = "清空历史记录";
+        }
+        else {
+            historyList = new String[historyCounts+1];
+            historyList[historyCounts] = "清空历史记录";
+        }
+        for (int i = 0; i < historyCounts; i++){
+            historyList[i] = sharedPreferences.getString("Key"+String.valueOf(historyCounts-i), "");
+        }
+        listPopupWindow = new ListPopupWindow(getContext());
+        listPopupWindow.setAdapter(new ArrayAdapter<String>(getActivity(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, historyList));
+        listPopupWindow.setAnchorView(editText);
+        listPopupWindow.setModal(true);
+
+        listPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i == historyList.length-1){
+                    editor.clear();
+                    editor.commit();
+                    historyList = new String[1];
+                    historyList[0] = "清空历史记录";
+                }
+                else {
+                    editText.setText(historyList[i]);
+                }
+
+                listPopupWindow.dismiss();
+            }
+        });
+        listPopupWindow.show();
     }
 
 }
