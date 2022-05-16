af513408b5c40b562e271eb0baaead7c728c9648
xcsh <zhangweikun_xcsh@163.com>
2022-05-16 10:28:45 +0800

    完成了详情页界面布局以及item的编写

--- app/src/main/java/com/example/foodcommentmp/Activitys/RestaurantDetailActivity.java
+++ app/src/main/java/com/example/foodcommentmp/Activitys/RestaurantDetailActivity.java
@@ -9,23 +9,23 @@ import android.widget.TextView;
 
 import com.example.foodcommentmp.R;
 
+// todo 要写四个item，分别为招牌菜、标签、评价、同类型餐厅
 public class RestaurantDetailActivity extends AppCompatActivity {
 
-    private TextView textView;
+    private String restaurantNameStr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_detail);
 
-        textView = findViewById(R.id.text_view);
 
         final Intent getIntent = getIntent();
         Bundle bundle = getIntent.getBundleExtra("data");
         if (bundle != null){
-            String get = bundle.getString("restaurant_name");
-            if(get != null){
-                textView.setText(get);
+            restaurantNameStr = bundle.getString("restaurant_name");
+            if(restaurantNameStr != null){
+                // todo 这里用LiveData去调
             }
         }
     }
---
+++ app/src/main/java/com/example/foodcommentmp/ViewModel/RestaurantDetailViewModel.java
@@ -0,0 +1,10 @@
+package com.example.foodcommentmp.ViewModel;
+
+import androidx.lifecycle.ViewModel;
+
+/**
+ * @author: zhangweikun
+ * @create: 2022-05-16 9:06
+ */
+public class RestaurantDetailViewModel extends ViewModel {
+}
